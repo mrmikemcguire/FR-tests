cd77d70ded9371e7768839fdd9d006b350fad3b1
mrmikemcguire <mcguire@mullenhigh.com>
2015-01-25 20:42:41 -0700

    Initial commit

---
+++ FR tests/src/AnagramChecker.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class AnagramChecker
+	{
+	public static void main(String[] args)
+		{
+		isAnagram("peach", "cheap");
+		}
+	
+	public static void isAnagram(String word1, String word2)
+		{
+		ArrayList<String> a = new ArrayList<String>();
+		ArrayList<String> b = new ArrayList<String>();
+		// for the very OCD amongst us
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		
+		for(int i = 0; i < word1.length(); i++)
+			{
+			a.add(word1.substring(i, i + 1));
+			}
+		
+		for(int i = 0; i < word2.length(); i++)
+			{
+			b.add(word2.substring(i, i + 1));
+			}
+		
+		Collections.sort(a);
+		Collections.sort(b);
+
+		if (a.equals(b))
+			{
+			System.out.println("It's an anagram.");
+			}
+		else
+			{
+			System.out.println("It's not an anagram.");
+			}
+		}
+	}
---
+++ FR tests/src/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RemoveDuplicates
+	{
+
+	public static void main(String[] args)
+		{
+		removeDuplicates("moose");
+		}
+	
+	public static void removeDuplicates(String word)
+		{
+		ArrayList<String> a = new ArrayList<String>();
+		
+		for(int i = 0; i < word.length(); i++)
+			{
+			a.add(word.substring(i, i + 1));
+			}
+
+		Collections.sort(a);
+		
+		for (int i = a.size() - 1; i > 0; i--)
+			{
+			if (a.get(i).equals(a.get(i - 1)))
+				{
+				a.remove(i);
+				}
+			}
+		
+		for (String s : a)
+			{
+			System.out.println(s);
+			}
+		}
+
+	}
